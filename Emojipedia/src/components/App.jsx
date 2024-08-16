3571698d3cbae7deb4b0a90abbb49e997c846dfb
Sibi-Sambasivam <151834749+Sibi-Sambasivam@users.noreply.github.com>
2024-08-16 23:35:07 +0530

    Add files via upload

---
+++ Emojipedia/src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Entry from "./Entry";
+
+function createEntry(emojipedia) {
+  return (
+    <Entry
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      {emojipedia.map(createEntry)}
+    </div>
+  );
+}
+
+export default App;
